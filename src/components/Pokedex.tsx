9e07ce26607158be8dd4bf62b26ee58ac83b37a8
M3chro <jakub.havel7@gmail.com>
2023-04-07 14:35:09 +0200

    Updated package info using 'npm install <package_name> --save'

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -5,43 +5,43 @@ import Pokemon from "./Pokemon";
 import PokedexSearchbar from "./PokemonSearchbar";
 
 const Pokedex = () => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [pokemon, setPokemon] = useState<Pokemon>();
-    const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [pokemon, setPokemon] = useState<Pokemon>();
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const url = `https://pokeapi.co/api/v2/pokemon/${searchTerm}`;
+  const url = `https://pokeapi.co/api/v2/pokemon/${searchTerm}`;
 
-    useEffect(() => {
-        if (!searchTerm) return;
+  useEffect(() => {
+    if (!searchTerm) return;
 
-        setLoading(true);
-        setError(false);
+    setLoading(true);
+    setError(false);
 
-        axios
-            .get(url)
-            .then((response) => {
-                setPokemon(response.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setError(true);
-                setLoading(false);
-            });
-    }, [searchTerm]);
+    axios
+      .get(url)
+      .then((response) => {
+        setPokemon(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  }, [searchTerm]);
 
-    const handleSearch = (value: string) => {
-        setSearchTerm(value.toLowerCase());
-    };
+  const handleSearch = (value: string) => {
+    setSearchTerm(value.toLowerCase());
+  };
 
-    return (
-        <>
-            <PokedexSearchbar onSearch={handleSearch} />
-            {loading && !pokemon && <p>Loading...</p>}
-            {error && <p>Something went wrong. Please try again later.</p>}
-            {pokemon && <PokemonCard pokemon={pokemon} />}
-        </>
-    );
+  return (
+    <>
+      <PokedexSearchbar onSearch={handleSearch} />
+      {loading && !pokemon && <p>Loading...</p>}
+      {error && <p>Something went wrong. Please try again later.</p>}
+      {pokemon && <PokemonCard pokemon={pokemon} />}
+    </>
+  );
 };
 
 export default Pokedex;
